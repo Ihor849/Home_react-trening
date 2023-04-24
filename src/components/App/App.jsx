9e20f60f7419c6e6ae7901feb360e8311cc16581
Ihor849 <333marchelo333@gmail.com>
2023-04-24 15:46:43 +0300

    Hom1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,24 @@
-const App = () => {
+import event from '../json/user.json'
+import statistics from '../json/statistics.json'
+import friends from '../json/friends.json'
+import transactions from '../json/transactions.json'
+import { Profile } from 'components/Profile/Profile'
+import { Statistics } from 'components/Statistics/Statistics.jsx'
+import { FriendList } from 'components/FriendList/FriendList.jsx'
+import {TransactionHistory} from 'components/TransactionHistory/TransactionHistory.jsx'
+import { Container } from './App.styled'
+
+export const App = () => {
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile users={event} />
+      <Statistics statistics={statistics} title="Upload stats" />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
+    </Container>
   );
-};
-export default App
+ 
+}
+
+

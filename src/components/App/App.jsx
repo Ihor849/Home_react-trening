8491525ce8bbc1de05193aafbcae2edaf0209c28
Ihor849 <333marchelo333@gmail.com>
2023-04-25 16:05:53 +0300

    todo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,111 @@
 import React, { Component } from 'react';
+import shortid from 'shortid';
+
 // import Counter from 'components/Counter';
 // import Drobdown from 'components/Drobdown';
-import ColorPicker from 'components/ColorPicker';
-// import TodoList from 'components/TodoList';
+// import ColorPicker from 'components/ColorPicker';
+import Container from 'components/Container/Container'
+import TodoEditor from '../TodoEditor/TodoEditor';
+import TodoList from 'components/TodoList';
+import Filter from '../Filter';
 
-// import initialTodos from 'components/json/todos.json';
+import initialTodos from 'components/json/todos.json';
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
 class App extends Component {
-  // state = {
-  //   todos: initialTodos,
-  // };
+  state = {
+    todos: initialTodos,
+    filter: '',
+  };
+  addTodo = text => {
+    const todo = {
+      id: shortid.generate(),
+      text,
+      completed: false,
+    };
+    this.setState(({ todos }) => ({
+      todos: [todo, ...todos],
+    }));
+  }
+  
 
-  // deleteTodo = todoId => {
-  //   this.setState(prevState => ({
-  //     todos: prevState.todos.filter(todo => todo.id !== todoId),
-  //   }));
-  // };
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+  
+  toggleCompleted = todoId => {
+    console.log(todoId);
+
+    // this.setState(prevState => ({
+    //   todos: prevState.todos.map(todo => {
+    //     if (todo.id === todoId) {
+    //       console.log("Нашли нужный Id !!!");
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed,
+    //       }
+    //     }
+    //     return todo;
+    //   }),
+      
+    // }));
+    this.setState(({ todos }) => ({
+      todos: todos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleTodos = () => {
+    const { filter, todos } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return todos.filter(todo =>
+      todo.text.toLowerCase().includes(normalizedFilter),
+    );
+  };
+  
+  calculateCompletedTodos = () => {
+    const { todos } = this.state;
+  }
 
   render() {
-    // const { todos } = this.state;
+    const { todos, filter } = this.state;
+    const totalTodoCount = todos.length;
+    const completedTodoCount = this.calculateCompletedTodos();
+    const visibleTodos = this.getVisibleTodos();
     return (
-      <>
-        <h1>Состояние компонента</h1>
+      <Container>
+        <h1>Состояние компонента TODO</h1>
         {/* <Counter initialValue={0} /> */}
         {/* <Drobdown /> */}
-        <ColorPicker options={colorPickerOptions} />
-        {/* <TodoList todos={todos} onDeleteTodo={this.deleteTodo} /> */}
-      </>
+        {/* <ColorPicker options={colorPickerOptions} /> */}
+
+        <TodoEditor onSubmit={this.addTodo} />
+
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
+      </Container>
+
     );
   }
 }
